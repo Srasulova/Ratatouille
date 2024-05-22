edd355b5fc926b4e73907318032b9f9868c7d649
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-05-21 20:46:21 -0400

    add db connection

--- app.py
+++ app.py
@@ -2,15 +2,71 @@ from flask import Flask, render_template, request, flash, redirect, session, g, 
 from flask_debugtoolbar import DebugToolbarExtension
 import os
 import requests
+from models import db, connect_db, User, Restaurant, VisitedRestaurants, WishlistRestaurants, Favorites, Review
+from sqlalchemy.exc import IntegrityError
+from forms import UserAddForm, UserEditForm, LoginForm, ReviewForm
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{app.root_path}/TwitterClone.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{app.root_path}/Ratatouille.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
+
+
+CURR_USER_KEY = 'curr_user'
+
+with app.app_context():
+   connect_db(app)
+
+
+@app.before_request
+def add_user_to_g():
+    """If a user is logged in, add them to Flask global"""
+
+
+
+
+
+
+API_KEY = "AIzaSyDQaQ4Zi8e-5YKSb_9VJvkKns3uYoq435g"
+
+@app.route('/')
+def homepage():
+    """Show homepage"""
+  
+    
+    return render_template("base.html")
+
+
+
+
+#   if request.method == "POST":
+#         place = request.form.get("place")
+#         print(place)
+
+#         url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+{place}&key={API_KEY}"
+#         response = requests.get(url)
+
+#         if response.status_code == 200:
+#             data = response.json()
+#             restaurants = data.get("results", [])
+#             # print(len(restaurants))
+#             # print(data)
+#         else:
+#             data = None
+#             print(f"Error: {response.status_code}")
+        
+        
+#         return render_template("base.html", restaurants=restaurants)
+
+
+
 
 
 # headers = {
@@ -20,29 +76,3 @@ toolbar = DebugToolbarExtension(app)
 
 # url = f"https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/{zipcode}/0"
 # response = requests.get(url, headers=headers)
-
-API_KEY = "AIzaSyDQaQ4Zi8e-5YKSb_9VJvkKns3uYoq435g"
-
-@app.route('/', methods=["GET", "POST"])
-def homepage():
-    """Show homepage"""
-    if request.method == "POST":
-        place = request.form.get("place")
-        print(place)
-
-        url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+{place}&key={API_KEY}"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            restaurants = data.get("results", [])
-            # print(len(restaurants))
-            # print(data)
-        else:
-            data = None
-            print(f"Error: {response.status_code}")
-        
-        
-        return render_template("base.html", restaurants=restaurants)
-    
-    return render_template("base.html")
