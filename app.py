bad18fa1449633cd5efc52e6149b331bef9c07ec
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-05-23 08:09:52 -0400

    implement both signup and login in the homepage route

--- app.py
+++ app.py
@@ -58,21 +58,46 @@ API_KEY = "AIzaSyDQaQ4Zi8e-5YKSb_9VJvkKns3uYoq435g"
 
 @app.route('/', methods = ['GET', 'POST'])
 def homepage():
-    """Show homepage and display the signup form if user clicks on the signup button"""  
-    form = UserAddForm()
+    """Show homepage and display the signup or login form based on user interaction"""  
+    signup_form = UserAddForm()
+    login_form = LoginForm()
 
-    if form.validate_on_submit():
-        try:
-            user = User.signup(username = form.username.data, password = form.password.data, email= form.email.data, image_url= form.image_url.data or User.image_url.default.arg)
-            db.session.commit()
-        except IntegrityError:
-            flash("This username already exists! So many foodies think alike. Try another one!")
-            return render_template('base.html', form=form)
-        do_login(user)
+    if request.method == 'POST':
+        form_type = request.form.get('form_type')
+        if form_type == 'signup' and signup_form.validate_on_submit():
+            try:
+                user = User.signup(username = signup_form.username.data, password = signup_form.password.data, email= signup_form.email.data, image_url= signup_form.image_url.data or User.image_url.default.arg)
+                db.session.commit()
+            except IntegrityError:
+                flash("This username already exists! So many foodies think alike. Try another one!")
+                return render_template('base.html', signup_form=signup_form, login_form=login_form)
+            do_login(user)
 
-        return redirect('place_search.html')
-    else:
-        return render_template('base.html', form=form)
+            return redirect('place_search.html')
+        elif form_type == 'login' and login_form.validate_on_submit():
+            user = User.authenticate(login_form.username.data, login_form.password.data)
+
+            if user:
+                do_login(user)
+                flash(f"Hello, {user.username}!")
+                return redirect('place_search.html')
+            else:
+                flash("Invalid credentials!")
+                return render_template('base.html', signup_form=signup_form, login_form=login_form)
+        
+    return render_template('base.html', signup_form=signup_form, login_form=login_form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    session.clear()
+    flash("You have been logged out")
+    return redirect("/")
+
+
+
     
 
 
